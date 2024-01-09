71c22244ae9848dfffde7bd4eaec350b46731caa
Bjørn Sandvik <bjorn@mastermaps.com>
2024-01-09 12:38:49 +0100

    chore: example period change

--- src/DataFetch.js
+++ src/DataFetch.js
@@ -7,8 +7,8 @@ const datasetParams = {
   datasetId: "ECMWF/ERA5_LAND/DAILY_AGGR",
   band: "temperature_2m",
   reducer: "mean",
-  startDate: "2023-01-01",
-  endDate: "2023-02-01", // not including
+  startDate: "2023-12-15",
+  endDate: "2024-01-01", // not including
   valueParser: (v) => Math.round((v - 273.15) * 10) / 10, // kelvin to celsius with one decimal
 };
 
