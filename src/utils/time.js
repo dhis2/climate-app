a3133b7bb391ce5640cfc44f1e9e1fe66b1b3120
Bjørn Sandvik <bjorn@dhis2.org>
2024-02-27 15:38:45 +0100

    Time fix

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,7 +1,21 @@
-const endDate = new Date();
-const startDate = new Date().setFullYear(endDate.getFullYear() - 1);
+/**
+ * Formats a date string, timestamp or date array into format used by DHIS2 and <input> date
+ * @param {Date} date
+ * @returns {String}
+ */
+export const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  return `${year}-${month}-${day}`; // xxxx-xx-xx
+};
+
+const currentYear = new Date().getFullYear();
+const lastYear = currentYear - 1;
+const endDate = new Date(`${lastYear}-12-31`);
+const startDate = new Date(endDate.getFullYear(), endDate.getMonth() - 11, 1); // 12 months back
 
 export const defaultPeriod = {
-  startDate: new Date(startDate).toISOString().slice(0, 10),
-  endDate: endDate.toISOString().slice(0, 10),
+  startDate: formatDate(startDate),
+  endDate: formatDate(endDate),
 };
