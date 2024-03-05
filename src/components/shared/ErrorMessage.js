f5cd76a104ff327e244c66feeed5535c4cef5bc9
Bjørn Sandvik <bjorn@dhis2.org>
2024-03-05 10:26:13 +0100

    Version bump

--- src/components/shared/ErrorMessage.js
+++ src/components/shared/ErrorMessage.js
@@ -2,9 +2,7 @@ import PropTypes from "prop-types";
 import styles from "./styles/ErrorMessage.module.css";
 
 const ErrorMessage = ({ error }) => (
-  <div className={styles.container}>
-    <p>{error.message}</p>
-  </div>
+  <div className={styles.container}>{error.message}</div>
 );
 
 ErrorMessage.propTypes = {
