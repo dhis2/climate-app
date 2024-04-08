1d584facd06c8c71c5214b515d0d745a58b8f181
Bjørn Sandvik <bjorn@dhis2.org>
2024-04-08 16:24:25 +0200

    Style fix

--- src/components/settings/SettingsPage.js
+++ src/components/settings/SettingsPage.js
@@ -1,13 +1,13 @@
 import i18n from "@dhis2/d2-i18n";
 import StartPageSelect from "./StartPageSelect";
 import TimeZoneSelect from "./TimeZoneSelect";
-
 import styles from "./styles/SettingsPage.module.css";
 
 const SettingsPage = () => {
   return (
     <div className={styles.container}>
       <h1>{i18n.t("App settings")}</h1>
+      <p>{i18n.t("Changes made below will apply to all users of this app.")}</p>
       <StartPageSelect />
       <TimeZoneSelect />
     </div>
