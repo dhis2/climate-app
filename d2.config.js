425cea280158d7bf1a03089980a58cfabd37dfab
Edoardo Sabadelli <edoardo@dhis2.org>
2024-06-19 15:43:26 +0200

    feat: enable dashboard plugin entry point

--- d2.config.js
+++ d2.config.js
@@ -1,9 +1,10 @@
 const config = {
-  type: "app",
-  title: "Climate Data",
-  entryPoints: {
-    app: "./src/App.js",
-  },
+    type: "app",
+    title: "Climate Data",
+    entryPoints: {
+        app: "./src/App.js",
+        plugin: "./src/DashboardPlugin.js",
+    },
 };
 
 module.exports = config;
