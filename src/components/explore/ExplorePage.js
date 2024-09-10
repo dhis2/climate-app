a250d53d61345ad06987a6308149a6840611e457
Bjørn Sandvik <bjorn@dhis2.org>
2024-09-10 11:40:00 +0200

    Code cleaning

--- src/components/explore/ExplorePage.js
+++ src/components/explore/ExplorePage.js
@@ -3,11 +3,9 @@ import styles from "./styles/ExplorePage.module.css";
 
 const ExplorePage = () => (
   <div className={styles.container}>
-    <div className={styles.introduction}>
-      <h1>{i18n.t("Explore weather and climate data")}</h1>
-      <p>{i18n.t("Select an organisation unit in the left panel")}</p>
-      <img src="images/explore.png" alt="Explore data screenshots" />
-    </div>
+    <h1>{i18n.t("Explore weather and climate data")}</h1>
+    <p>{i18n.t("Select an organisation unit in the left panel")}</p>
+    <img src="images/explore.png" alt="Explore data screenshots" />
   </div>
 );
 
