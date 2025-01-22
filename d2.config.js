b899c105d2db7639b64173e5607c7ea69846ec6f
Kai Vandivier <kpvandivier@gmail.com>
2025-01-22 11:57:14 +0100

    chore: add min dhis2 version

--- d2.config.js
+++ d2.config.js
@@ -2,6 +2,7 @@ const config = {
     type: 'app',
     title: 'DHIS2 Climate App',
     id: 'effb986c-a3c7-485e-a2f6-5e54ff9df7c3',
+    minDHIS2Version: '2.37',
 
     entryPoints: {
         app: './src/App.js',
