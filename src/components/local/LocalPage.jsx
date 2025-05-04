42fe34d729ed91e0d6bbb4b0070ccbf38abdc152
Karim Bahgat <karimba@uio.no>
2025-05-04 23:24:34 +0200

    chore: add local page image

--- src/components/local/LocalPage.jsx
+++ src/components/local/LocalPage.jsx
@@ -3,9 +3,9 @@ import styles from './styles/LocalPage.module.css'
 
 const LocalPage = () => (
     <div className={styles.container}>
-        <h1>{i18n.t('Explore local weather and climate data')}</h1>
+        <h1>{i18n.t('Local weather and climate data')}</h1>
         <p>{i18n.t('Select an organisation unit in the left panel')}</p>
-        <img src="images/explore.png" alt="Local data screenshots" />
+        <img src="images/local.png" alt="Local data screenshots" style={{maxHeight: '75vh'}} />
     </div>
 )
 
