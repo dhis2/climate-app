73276402dc034809aaea3b30d0aa3f64aad7ca14
Bjørn Sandvik <bjorn@dhis2.org>
2025-05-27 13:49:53 +0200

    fix: setup page improvements

--- src/components/setup/DataSet.jsx
+++ src/components/setup/DataSet.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 const DataSet = ({ name, shortName, periodType }) => (
     <>
+        <h2>{i18n.t('Assign to a data set')}</h2>
         <p>
             {i18n.t(
                 'If you import daily data, you should assign the data elements to a daily data set (weather and climate). For weekly or monthly data, assign to a weekly or monthly data set (vegetation index). Assigning to a data set helps to maintain a quality metadata and data where integrity, consistency, approvals and protection are facilitated. How exactly to define such a data set depends on the available users and user groups. Similarly, which of the organisation units can collect this daily climate data set depends on situated contexts. However, since we can provide data for any administrative unit that has defined geographic boundaries we leave that decision to users. Most probable choice is all organisation units in your system.'
--- src/components/setup/SetupPage.jsx
+++ src/components/setup/SetupPage.jsx
@@ -13,17 +13,7 @@ const SetupPage = () => {
             <h1>{i18n.t('How to configure this app')}</h1>
             <p>
                 {i18n.t(
-                    'Before you can import data, you need to make some configurations in the Maintenance app.'
-                )}
-            </p>
-            <p>
-                {i18n.t(
-                    "When daily data is imported it can be aggregated to other period types in DHIS2. We don't recommend aggregating across organisation unit levels, especially when going from facility level to higher levels defined by a geographic boundary. To make sure the values remain at its own level, assign all org unit levels as Aggregation levels for each data element created."
-                )}
-            </p>
-            <p>
-                {i18n.t(
-                    'Create DHIS2 data elements for the data you want to import. We recommend including the data source in the name to distinguish it from other sources (e.g. precepetation data is available from both ERA5-Land and CHIRPS, and there will be additional data providers in the future). If you use the same code specified below, we will preselect the data element in the import interface.'
+                    'Before you can import data, you need to make some configurations in the Maintenance app. For detailed instructions on how to configure DHIS2, choose which data you would like to import below.'
                 )}
             </p>
             <Dataset
@@ -32,6 +22,17 @@ const SetupPage = () => {
                 onChange={setDataset}
                 showDescription={false}
             />
+            <h2>{i18n.t('Create the data element')}</h2>
+            <p>
+                {i18n.t(
+                    'Create DHIS2 data elements only for the data you want to import. We recommend including the data source in the name to distinguish it from other sources (e.g. precepetation data is available from both ERA5-Land and CHIRPS, and there will be additional data providers in the future). If you use the same code specified below, we will preselect the data element in the import interface.'
+                )}
+            </p>
+            <p>
+                {i18n.t(
+                    "When daily data is imported it can be aggregated to other period types in DHIS2. We don't recommend aggregating across organisation unit levels, especially when going from facility level to higher levels defined by a geographic boundary. To make sure the values remain at its own level, assign all org unit levels as Aggregation levels for each data element created."
+                )}
+            </p>
             {dataset && <DataElement {...dataset} />}
         </div>
     )
