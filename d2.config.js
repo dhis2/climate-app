70b38bd2b0ea295d00834bee600ae6035ab4c1a2
Jen Jones Arnesen <jennifer@dhis2.org>
2026-04-30 11:25:16 +0200

    fix: update supported version to 2.40

--- d2.config.js
+++ d2.config.js
@@ -2,7 +2,7 @@ const config = {
     type: 'app',
     title: 'DHIS2 Climate App',
     id: 'effb986c-a3c7-485e-a2f6-5e54ff9df7c3',
-    minDHIS2Version: '2.37',
+    minDHIS2Version: '2.40',
 
     entryPoints: {
         app: './src/App.jsx',
