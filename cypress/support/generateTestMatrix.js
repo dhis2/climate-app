ae96c92f70a5e7e545d524f1dc2ea869c1502af0
Jen Jones Arnesen <jennifer@dhis2.org>
2026-05-14 11:41:45 +0200

    chore: code smell

--- cypress/support/generateTestMatrix.js
+++ cypress/support/generateTestMatrix.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('node.fs')
+const path = require('node.path')
 
 const getAllFiles = (dirPath, arrayOfFiles = []) => {
     const files = fs.readdirSync(dirPath)
